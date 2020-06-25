5d8ccba9d70bab692fa58db9409e91603c43e333
Lucas Viana <lucasviana112@gmail.com>
2020-06-25 01:20:27 -0300

    estruturação do componente principal do app, chamada dos componentes do cabeçalho e rota, englobando store do redux sobre os componentes, chamada container para uso do toast

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'react-toastify/dist/ReactToastify.css';
+import { BrowserRouter } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import { Provider } from 'react-redux';
 
-function App() {
+import store from './store';
+
+import Routes from './routes';
+import Header from './components/Header';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Header />
+        <Routes />
+        <ToastContainer position="bottom-right" autoClose={3000} />
+      </BrowserRouter>
+    </Provider>
   );
-}
+};
 
 export default App;
