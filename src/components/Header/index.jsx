b3cc354f740816754bcb154a8336f727b0c85e2b
Lucas Viana <lucasviana112@gmail.com>
2020-06-25 12:52:23 -0300

    ajuste layout menu de navegação, inserção de logo

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,12 +3,15 @@ import './styles.scss';
 
 import { Link } from 'react-router-dom';
 
+import logo from '../../assets/camera_old.png';
+
 const Header = () => {
   return (
     <header className="header">
-      <nav className="header__logo">
+      <nav className="header__nav">
         <Link to="/">
-          <span>LOGO</span>
+          <img src={logo} alt="Logo" />
+          <span>LOCADORA</span>
         </Link>
         <Link to="/movie">cadastro</Link>
       </nav>
