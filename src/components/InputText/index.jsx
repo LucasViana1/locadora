02f170d3bf6373b20e3b92df081c1a5727c11d7b
Lucas Viana <lucasviana112@gmail.com>
2020-06-25 01:23:51 -0300

    componente dos input de texto do formulario

---
+++ src/components/InputText/index.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import './styles.scss';
+
+const InputText = ({
+  label,
+  name,
+  value,
+  handleValue,
+  placeholder,
+  textarea,
+  error,
+}) => {
+  return (
+    <div className="field">
+      <label htmlFor={name}>{label}</label>
+      {textarea ? (
+        <textarea
+          name={name}
+          id={name}
+          placeholder={placeholder}
+          value={value}
+          onChange={(event) => handleValue(event.target.value)}
+          cols="30"
+          rows="4"
+        ></textarea>
+      ) : (
+        <input
+          type="text"
+          name={name}
+          id={name}
+          placeholder={placeholder}
+          value={value}
+          onChange={(event) => handleValue(event.target.value)}
+        />
+      )}
+      {/* {console.log(error)}
+      {console.log(name)} */}
+      {error === name ? <span>Campo obrigatório</span> : null}
+    </div>
+  );
+};
+
+export default InputText;
