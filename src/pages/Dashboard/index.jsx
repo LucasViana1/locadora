1e0744e7a78bb7515d25bbd0f4cd1f0de4844592
Lucas Viana <lucasviana112@gmail.com>
2020-06-26 11:49:58 -0300

    inserido funcionalidade de carregamento do catalogo e verificação caso não encontre nenhum registro

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -7,36 +7,43 @@ import { requestMovies } from '../../store/modules/movies/action';
 
 import Film from '../../components/Film';
 import { MdMoodBad } from 'react-icons/md';
+import Loading from '../../components/Loading';
 
 const Dashboard = () => {
+  const [noRegistry, setNoRegistry] = useState(false);
+
   const dispatch = useDispatch();
 
   const { movies } = useSelector((state) => state.movies);
 
   useEffect(() => {
-    api.get('movies').then((response) => {
-      console.log(response.data);
-      dispatch(requestMovies(response.data));
-    });
+    api
+      .get('movies')
+      .then((response) => {
+        dispatch(requestMovies(response.data));
+      })
+      .catch((error) => setNoRegistry(true));
   }, [dispatch]);
 
   return (
-    <section className="dashboard">
-      <h1>Catálogo de filmes</h1>
+    <>
+      <section className="dashboard">
+        <h1>Catálogo de filmes</h1>
 
-      <article className="dashboard__container">
-        {movies.length !== 0 ? (
-          movies.map((movie) => <Film key={movie.id} film={movie} />)
-        ) : (
-          <span className="dashboard__alert">
-            Nenhum filme encontrado!{' '}
-            <MdMoodBad className="dashboard__alert--icon" />
-          </span>
-        )}
-        {console.log('movies')}
-        {console.log(movies)}
-      </article>
-    </section>
+        <article className="dashboard__container">
+          {movies.length !== 0 &&
+            movies.map((movie) => <Film key={movie.id} film={movie} />)}
+
+          {noRegistry && (
+            <span className="dashboard__alert">
+              Nenhum filme encontrado!{' '}
+              <MdMoodBad className="dashboard__alert--icon" />
+            </span>
+          )}
+        </article>
+      </section>
+      {!noRegistry && movies.length === 0 && <Loading size={100} back />}
+    </>
   );
 };
 
